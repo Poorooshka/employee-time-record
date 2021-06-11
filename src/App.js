1fb35b0777c555ec1fd14e50fcb1b08d8a819644
Nashmil <71593756+Poorooshka@users.noreply.github.com>
2021-06-11 11:11:45 -0700

    shift overview form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <label>
+          Name
+          <input value="" />
+        </label>
+        <label>
+          Start Time
+          <input type="datetime-local" value="" />
+        </label>
+        <label>
+          End Time
+          <input type="datetime-local" value="" />
+        </label>
+        <button>Save</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
