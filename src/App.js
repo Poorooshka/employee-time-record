ad382df46eacee444af6ae79285e2b5d7ee27f66
Nashmil <71593756+Poorooshka@users.noreply.github.com>
2021-06-11 11:28:49 -0700

    adding states to the form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  const handleNameInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleStartTimeInputChange = (event) => {
+    setStartTime(event.target.value);
+  };
+
+  const handleEndTimeInputChange = (event) => {
+    setEndTime(event.target.value);
+  };
+
   return (
     <div className="App">
       <div>
         <label>
           Name
-          <input value="" />
+          <input value={name} onChange={handleNameInputChange} />
         </label>
         <label>
           Start Time
-          <input type="datetime-local" value="" />
+          <input
+            type="datetime-local"
+            value={startTime}
+            onChange={handleStartTimeInputChange}
+          />
         </label>
         <label>
           End Time
-          <input type="datetime-local" value="" />
+          <input
+            type="datetime-local"
+            value={endTime}
+            onChange={handleEndTimeInputChange}
+          />
         </label>
         <button>Save</button>
       </div>
